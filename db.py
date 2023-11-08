66841a69291891812c9666e25da4622ab22d9f33
Matthew Dailis <matthew.l.dailis@jpl.nasa.gov>
2023-11-07 20:52:38 -0800

    Remove hard-coded cached directory

--- db.py
+++ db.py
@@ -1,13 +1,34 @@
+from pathlib import Path
 import json
 
-db_location = "/Users/dailis/.pm/pm.json"
+pm_directory = Path.home() / ".pm"
+db_location = pm_directory / "pm.json"
+
+
+empty_db = {
+    "issues": [],
+    "prs": [],
+    "project_items": {
+        "items": []
+    }
+}
 
 
 def retrieve():
+    ensure_pm_directory_exists()
     with open(db_location, "r") as f:
         return json.load(f)
 
 
 def store(contents):
+    ensure_pm_directory_exists()
     with open(db_location, "w") as f:
         json.dump(contents, f)
+
+
+def ensure_pm_directory_exists():
+    pm_directory.mkdir(parents=True, exist_ok=True)
+    if not db_location.is_file():
+        print("Creating empty database at" , db_location)
+        with open(db_location, "w") as f:
+            json.dump(empty_db, f)
